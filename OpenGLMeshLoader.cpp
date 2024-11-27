036dc5c867a81c42ad3bb414e42bd24f5d48443f
demiana <demianareffaat@gmail.com>
2024-11-27 11:58:53 +0200

    test

--- OpenGLMeshLoader.cpp
+++ OpenGLMeshLoader.cpp
@@ -21,7 +21,7 @@ public:
 	GLdouble x, y, z;
 	Vector() {}
 	Vector(GLdouble _x, GLdouble _y, GLdouble _z) : x(_x), y(_y), z(_z) {}
-	//================================================================================================//
+	//==========================================================================//
 	// Operator Overloading; In C++ you can override the behavior of operators for you class objects. //
 	// Here we are overloading the += operator to add a given value to all vector coordinates.        //
 	//================================================================================================//
